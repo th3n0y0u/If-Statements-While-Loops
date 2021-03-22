d1e8ccd9bd4fe25ec4536d3a9207060c24afdfde
soop <110252@eesd.org>
2021-03-22 23:26:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int ampro = 1;
+    while(ampro <= 69);
+     if(ampro <= 69);
+        ampro += 1;
+        System.out.println("sup am pro");
+        System.out.println(ampro);
+     if(ampro >= 69)
+        ampro -= 1;
+        System.out.println("rekt");
+  }
+}
